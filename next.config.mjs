b5c92e68c20cd6697878f925789629667683a4d1
llaryall <arya.xll17llx@gmail.com>
2025-05-31 19:21:58 +0330

    a little fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+// IMPORTANT: Make sure 'ReactJS-CheatSheet' is your actual GitHub repository name!
+const repositoryName = 'ReactJS-CheatSheet'; 
+
 const nextConfig = {
     output: 'export',
-    basePath: isProd ? 'ReactJS-CheatSheet' : '',
-    assetPrefix: isProd ? 'ReactJS-CheatSheet' : '',
-    
+    basePath: isProd ? `/${repositoryName}` : '',
+    assetPrefix: isProd ? `/${repositoryName}/` : '',
+    images: {
+        unoptimized: true, // Good for static exports, especially for GitHub Pages
+    },
 }
 
 export default nextConfig;
