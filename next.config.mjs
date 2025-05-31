c0a5088795257750749068c3c8c3fee8f5a48189
llaryall <arya.xll17llx@gmail.com>
2025-05-31 19:10:05 +0330

    config for gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-        unoptimized: true, // Disable Next.js image optimization
-    },
+    basePath: isProd ? 'ReactJS-CheatSheet' : '',
+    assetPrefix: isProd ? 'ReactJS-CheatSheet' : '',
+    
 }
 
 export default nextConfig;
